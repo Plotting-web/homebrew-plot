69361bfae587fab65e40e8785fbcd937a53dc293
joohyun.ha <mylifeforcoding@gmail.com>
2024-09-30 02:31:55 +0900

    feat: change command to plotdbconn and bump version to v0.0.2

--- Formula/plotdbconn.rb
+++ Formula/plotdbconn.rb
@@ -1,13 +1,13 @@
 class Plotdbconn < Formula
     desc "A script to open an SSM port forwarding session to an AWS EC2 instance and connect to an RDS database"
     homepage "https://github.com/mangchhe/homebrew-plot"
-    url "https://github.com/mangchhe/homebrew-plot/releases/download/v0.0.1/plotdbconn-0.0.1.tar.gz"
-    sha256 "5c91d9ee608e18be4d72e7b514b101d493f7995201b600e909d8b64bde0b9f82"
+    url "https://github.com/mangchhe/homebrew-plot/releases/download/v0.0.2/plotdbconn-0.0.2.tar.gz"
+    sha256 "2517a2ff01f06559262ee67b3aab93b45320dce9d88c0b95b4eea8991f18fbbe"
   
     depends_on "awscli"
   
     def install
-      bin.install "plotdbconn.sh"
+      bin.install "plotdbconn.sh" => "plotdbconn"
     end
   
     test do
