da74e27f409a3e0d3748dba4d3d3846385d94240
joohyun.ha <mylifeforcoding@gmail.com>
2024-10-09 20:01:01 +0900

    feat: modify to fetch active instances only and bump version to v0.0.4

--- Formula/plotdbconn.rb
+++ Formula/plotdbconn.rb
@@ -1,8 +1,8 @@
 class Plotdbconn < Formula
     desc "A script to open an SSM port forwarding session to an AWS EC2 instance and connect to an RDS database"
     homepage "https://github.com/mangchhe/homebrew-plot"
-    url "https://github.com/mangchhe/homebrew-plot/releases/download/v0.0.3/plotdbconn-0.0.3.tar.gz"
-    sha256 "eed0484f9159c5854a8269bb758502098dd0988f970d254eb1375db801b0f14b"
+    url "https://github.com/mangchhe/homebrew-plot/releases/download/v0.0.4/plotdbconn-0.0.4.tar.gz"
+    sha256 "51751d6581d2543fd19e0d65425a97a3a4f4d22089895732463e1ffde6b2371c"
   
     depends_on "awscli"
   
